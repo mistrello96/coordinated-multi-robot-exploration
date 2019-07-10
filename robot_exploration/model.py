d6cba97cdb62f380acb532272b53e7f926eda63d
Matteo Mistri <matteomistri1996@gmail.com>
2019-07-10 11:24:11 +0200

    Added weighted graph representation for the seen grid. Could exploit networkx for compute the SP in a efficent way

--- robot_exploration/model.py
+++ robot_exploration/model.py
@@ -4,6 +4,7 @@ from mesa.space import MultiGrid
 import numpy as np
 import math
 import heapq
+import networkx as nx
 
 class ExplorationArea(Model):
 	def __init__(self):
@@ -31,6 +32,8 @@ class ExplorationArea(Model):
   			grid_seen[i[1:]] = False
   			grid_priority[i[1:]] = False
   			grid_utility[i[1:]] = 1.0
+  			seen_graph = nx.DiGraph()
+  			seen_graph.add_node((0,0))
   			#TODO wifi representation
 
         # create agents
@@ -41,7 +44,8 @@ class ExplorationArea(Model):
 			#x = self.random.randrange(self.grid.width)
 			#y = self.random.randrange(self.grid.height)
 			x = 0
-			y = self.grid.height / 2
+			#y = self.grid.height / 2
+			y = 0
 			self.grid.place_agent(a, (x, y))
 
 	# what the model do at each time step
@@ -61,8 +65,8 @@ class Robot(Agent):
 		# self pos is already initializated
 		self.last_pos = self.pos
 		self.seen_radius = seen_radius
-		self.target_heap = list()
-		heapq.heapify(self.target_heap)
+		self.target_path = list()
+		heapq.heapify(self.target_path)
 		self.target_cell = ()
 		self.exploration_treshold = math.inf
 		self.exploration_status = 0
@@ -70,11 +74,30 @@ class Robot(Agent):
 
 	def step(self):
 		# if the agent has a move to get closer to the target, move towards it
-		if not list(self.target):
+		if not list(self.target_path):
+			# update the last position
 			self.last_pos = self.pos
-			self.model.grid.move_agent(self, heapq.heappop(target))
-			for n in self.model.grid.iter_neighborhood(self.pos, moore=True, include_center=False, radius=self.seen_radius):
-				self.model.grid_seen[n] = True
+			# move the agent
+			self.model.grid.move_agent(self, heapq.heappop(target_path))
+			# find the cell that the robot can see
+			for nearby in self.model.grid.iter_neighborhood(self.pos, moore=True, include_center=False, radius=self.seen_radius):
+				# TOREMOVE
+				self.model.grid_seen[nearby] = True
+				# s is suource node, d is destination node
+				s = self.pos
+				d = nearby
+				# check if d is reachable
+				if self.model.grid_traversability[d]:
+					# compute the cost of moving in that direction
+					w = 1 + (self.model.grid_difficulty[d] // 4 )
+					# if the edge is not yet presdent, add it
+					if tuple([s, d]) not in self.model.seen_graph.edges():
+						self.model.seen_graph.add_weighted_edges_from(tuple([s, d, w]))
+				s = nearby
+				d = self.pos
+				w = 1 + (self.model.grid_difficulty[d] // 4 )
+				if tuple([s, d]) not in self.model.seen_graph.edges():
+					self.model.seen_graph.add_weighted_edges_from(tuple([s, d, w]))
 			# TODO wifi range check
 		else:
 			# if the agent has no move to do, but is on the target, explore
@@ -89,7 +112,24 @@ class Robot(Agent):
 					self.target_cell = ()
 			# if the robot has no target, find one
 			else:
-				frontier_cells
+				# find the frontier cells
+				frontier_cells = list()
+				for cell in grid_explored.keys():
+					if grid_explored[cell] == False:
+						for element in self.model.get_neighborhood(cell, moore, include_center=False, radius=1):
+							if grid_explored[element] == True:
+								frontier_cells.append(cell)
+								break
+				# only consider seen cells that are not blocked for the SP computation
+				# list of tuples, the first element is the cell, the second is the cost
+				bids = list()
+
+
+				# pick the most convinient cell
+ 
+				# compute and store the most convinient path
+
+				# reduce the utility of the cells nearby the target cell
 				
 
 
