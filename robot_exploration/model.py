2d36ed4b8e8a06a5fd6167fcbd6b115c0788dd55
Matteo Mistri <matteomistri1996@gmail.com>
2019-07-11 17:06:32 +0200

    Fixed LOTS of bugs, now the model seems to work properly

--- robot_exploration/model.py
+++ robot_exploration/model.py
@@ -35,7 +35,7 @@ class ExplorationArea(Model):
 				self.grid_utility[i[1:]] = 1.0
 			else:
 				self.grid_difficulty[i[1:]] = math.inf
-				self.grid_explored[i[1:]] = True
+				self.grid_explored[i[1:]] = None
 				self.grid_priority[i[1:]] = False
 				self.grid_utility[i[1:]] = -math.inf
   			
@@ -44,22 +44,24 @@ class ExplorationArea(Model):
 
 		# create agents
 		for i in range(self.nrobots):
-			a = Robot(i, self, self.radar_radius)
-			self.schedule.add(a)
 			# Add the agent to a random grid cell??
 			#x = self.random.randrange(self.grid.width)
 			#y = self.random.randrange(self.grid.height)
 			x = 0
 			#y = self.grid.height / 2
 			y = 0
+			a = Robot(i, self, tuple([x,y]),self.radar_radius)
+			self.schedule.add(a)
 			self.grid.place_agent(a, (x, y))
+			self.grid_explored[tuple([x,y])] = True
 
 	# what the model do at each time step
 	def step(self):
 		# call step function for all of the robots in random order
 		self.schedule.step()
+		print(list(self.grid_explored.values()).count(True) / list(self.grid_explored.values()).count(False))
 
-	def run_model(self, n):
+	def run_model(self):
 		while(True):
 			if (False in self.grid_explored.values()):
 				self.step()
@@ -69,16 +71,18 @@ class ExplorationArea(Model):
 		# implement search untill victim found
 
 class Robot(Agent):
-	def __init__(self, unique_id, model, radar_radius):
+	def __init__(self, unique_id, model, pos, radar_radius):
 		super().__init__(unique_id, model)
 		# self pos is already initializated
 		self.last_pos = tuple()
+		self.pos = pos
 		self.radar_radius = radar_radius
 		self.target_path = list()
 		heapq.heapify(self.target_path)
 		self.target_cell = ()
 		self.exploration_treshold = math.inf
 		self.exploration_status = 0
+		self.percept()
 
 	def percept(self):
 		for nearby in self.model.grid.iter_neighborhood(self.pos, moore=True, include_center=False, radius=self.radar_radius):
@@ -91,19 +95,19 @@ class Robot(Agent):
 					w = 1 + (self.model.grid_difficulty[d] // 4 )
 					# if the edge is not yet present, add it
 					if tuple([s, d]) not in self.model.seen_graph.edges():
-						self.model.seen_graph.add_weighted_edges_from(tuple([s, d, w]))
+						self.model.seen_graph.add_edge(s, d, weight=w)
 				s = nearby
 				d = self.pos
 				w = 1 + (self.model.grid_difficulty[d] // 4 )
 				if tuple([s, d]) not in self.model.seen_graph.edges():
-					self.model.seen_graph.add_weighted_edges_from(tuple([s, d, w]))
+					self.model.seen_graph.add_edge(s, d, weight=w)
 	
 	def find_frontier_cells(self):
 		frontier_cells = list()
-		for cell in grid_explored.keys():
-			if not grid_explored[cell]:
-				for element in self.model.get_neighborhood(cell, moore, include_center=False, radius=1):
-					if grid_explored[element] and grid_traversability[element]:
+		for cell in self.model.grid_explored.keys():
+			if not self.model.grid_explored[cell]:
+				for element in self.model.grid.get_neighborhood(cell, "moore", include_center=False, radius=1):
+					if self.model.grid_explored[element]:
 						frontier_cells.append(cell)
 						break
 		return frontier_cells
@@ -116,7 +120,7 @@ class Robot(Agent):
 		bids = list()
 		for cell in frontier_cells:
 			try:
-				dist = nx.astar_path_length(seen_graph, self.pos, cell, weight='weight')
+				dist = nx.astar_path_length(self.model.seen_graph, self.pos, cell, weight='weight')
 				bids.append((cell, dist))
 			except:
 				# if the path is not known, the cell is not considered
@@ -140,7 +144,7 @@ class Robot(Agent):
 					best_cost = cost						
 		return best_cell
 
-	def distance(cell1, cell2):
+	def distance(self, cell1, cell2):
 		x1, y1 = cell1
 		x2, y2 = cell2
 		dx = abs(x2 - x1);
@@ -158,19 +162,19 @@ class Robot(Agent):
 
 	def step(self):
 		# if the agent has a move to get closer to the target, move towards it
-		if not list(self.target_path):
+		if list(self.target_path):
 			# update the last position
 			self.last_pos = self.pos
 			# move the agent
-			self.model.grid.move_agent(self, heapq.heappop(target_path))
+			self.model.grid.move_agent(self, heapq.heappop(self.target_path))
 			# find the cell that the robot can see
 			self.percept()
 			# TODO wifi range check
 		else:
 			# if the agent has no move to do, but is on the target, explore
-			if not target_cell:
-				if exploration_status < exploration_treshold:
-					exploration_status += 1
+			if self.target_cell:
+				if self.exploration_status < self.exploration_treshold:
+					self.exploration_status += 1
 				# if the agent has ended the exploration, update the data
 				else:
 					self.exploration_treshold = math.inf
@@ -181,13 +185,16 @@ class Robot(Agent):
 			else:
 				frontier_cells = self.find_frontier_cells()
 				self.target_cell = self.find_best_cell(frontier_cells)
-				self.exploration_treshold = self.model.grid_difficulty[best_cell]
-				# not sure if make sense
-				self.model.grid_utility[self.target_cell] = -math.inf
-				# compute and store the most convinient path to get there
-				self.target_path = nx.astar_path(self.model.seen_graph, self.pos, cell, weight='weight')
-				# reduce the utility of the FRONTIERS (and only frontiers) cells nearby the target cell
-				for element in self.model.get_neighborhood(self.target_cell, moore, include_center=False, radius=self.radar_radius):
-					if element in frontier_cells:
-						#self.model.grid_utility[elemet] -= (1 - self.disance(self.target, element) / self.radar_radius)
-						self.model.grid_utility[elemet] *= (1 - self.disance(self.target, element) / self.radar_radius)
+				# if no frontier is avaiable, just pass
+				if (self.target_cell):
+					self.exploration_treshold = self.model.grid_difficulty[self.target_cell]
+					# not sure if make sense
+					self.model.grid_utility[self.target_cell] = -math.inf
+					# compute and store the most convinient path to get there
+					self.target_path = nx.astar_path(self.model.seen_graph, self.pos, self.target_cell, weight='weight')
+					# reduce the utility of the FRONTIERS (and only frontiers) cells nearby the target cell
+					for element in self.model.grid.get_neighborhood(self.target_cell, "moore", include_center=False, radius=self.radar_radius):
+						if element in frontier_cells:
+							#self.model.grid_utility[elemet] -= (1 - self.distance(self.target_cell, element) / self.radar_radius)
+							self.model.grid_utility[element] *= (1 - self.distance(self.target_cell, element) / self.radar_radius)
+		print(self.pos)
