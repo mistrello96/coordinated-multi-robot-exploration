0b6a2fd0b18a0dafdb3932feda4b79c779854f25
Daniele-Papetti <d.papetti1@campus.unimib.it>
2019-07-14 09:54:24 +0200

    minors to server

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Coordinated Multi-Robot Exploration
 
-University projet that exploit MESA to simulate a search and rescue task carried out by cooperating robotic agents
+University projet which exploits MESA to simulate a search and rescue task carried out by cooperating robotic agents
--- robot_exploration/server.py
+++ robot_exploration/server.py
@@ -26,16 +26,16 @@ def agent_portrayal(agent):
 model_params = {
 # order of values is default, min, max, step
     "nrobots": UserSettableParameter('slider', "Number of agents", 5, 1, 100, 1,
-                                        description="Choose how many agents to include in the model"),
+                                        description = "Choose how many agents to include in the model"),
     "radar_radius":UserSettableParameter('slider', "Radar radius", 3, 0, 10, 1,
-                                        description="Choose how many cells around the robot can see"),
-    "ncells":UserSettableParameter('number', "Number of rows/columns of cells", value=10),
+                                        description = "Choose how many cells around the robot can see"),
+    "ncells":UserSettableParameter('number', "Number of rows/columns of cells", value = 10),
     "obstacles_dist": UserSettableParameter('slider', "Obstacle probability", 0.1, 0, 1, 0.01,
-                                        description="Choose how many obstacle there are in the map"),
+                                        description = "Choose how many obstacle there are in the map"),
     "wifi_range":UserSettableParameter('slider', "Wifi range", 50, 10, 1000, 10,
-                                        description="Choose how many cells around the robot can see"),
+                                        description = "Choose how many cells around the robot can see"),
     "alpha":UserSettableParameter('number', "Alpha value", value = 0.01,
-                                        description="Importance of path cost in target selection")
+                                        description = "Importance of path cost in target selection")
 }
 
 #grid representation
