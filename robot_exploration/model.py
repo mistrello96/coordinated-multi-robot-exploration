bc9a500bd60b9c6f34e5bbc5c491035f37d718d6
Daniele-Papetti <d.papetti1@campus.unimib.it>
2019-07-14 11:30:05 +0200

    added comments with DP in order to write down my doubts + minors to comments and beauty of code

--- robot_exploration/model.py
+++ robot_exploration/model.py
@@ -17,20 +17,22 @@ class ExplorationArea(Model):
 		self.alpha = alpha
 
 		# grid and schedule representation
-		self.grid = MultiGrid(ncells, ncells, torus=False)
+		self.grid = MultiGrid(ncells, ncells, torus = False)
 		self.schedule = RandomActivation(self)
 		# unique counter for agents 
 		self.agent_counter = 1
 		# place a cell agent on each chell of the grid
+
+		# DP What are their agent types? I mean, our agets were robot, were they?
 		for i in self.grid.coord_iter():
 			rand = np.random.random_sample()
 			traversability = True if rand > self.obstacles_dist else False
 			# if free
 			if traversability:
-				difficulty = np.random.randint(low = 1, high=13)
+				difficulty = np.random.randint(low = 1, high = 13)
 				explored = 0
 				priority = False
-				utility= 1.0
+				utility = 1.0
 			# if obstacle
 			else:
 				difficulty = math.inf
@@ -45,15 +47,26 @@ class ExplorationArea(Model):
   			
   		# graph of seen cells
 		self.seen_graph = nx.DiGraph()
+		
 		#TODO wifi representation
 
 		# create agents
+
+		# DP Why are we still talking about agents? We have already ceated them  
+		# I think we should use different terminology than before
+
+		# DP Why not range(self.nrobots)?
+		# Can't we use two different counters?
 		for i in range(self.agent_counter, self.nrobots + self.agent_counter):
 			y = self.random.randrange(self.grid.height)
+			# DP There should be a way to avoi that while, 
+			# in Python there's a function which randomize an element from
+			# a list; we can use that updating the list every time we place a robot
 			x = 0
 			# take the agent Cell in the grid cell x,y
 			cell = [obj for obj in self.grid.get_cell_list_contents(tuple([x,y])) if isinstance(obj, Cell)][0]
 			# do-while structure, place agents only in free cells
+			# DP Moment moment moment, does do-while still exist? 
 			while(cell.explored == -1):
 				y = self.random.randrange(self.grid.height)
 				cell = self.grid.get_cell_list_contents(tuple([x,y]))
@@ -65,27 +78,31 @@ class ExplorationArea(Model):
 			cell.explored = 2
 
 
-	# what the model do at each time step
+	# what the model does at each time step
 	def step(self):
 		# call step function for all of the robots in random order
 		self.schedule.step()
 		# compute percentage of explored over total 
 		count_explored = 0
-		count_obstacle = 0
+		count_obstacle = 0 # DP can't we know that a priori?
+		# DP we can calculate the number of obstacles when we initilize the model
+		# and save that number in an attribute of the model
 		# iterate over cells
 		for i in self.grid.coord_iter():
 			cell = [obj for obj in self.grid.get_cell_list_contents(i[1:]) if isinstance(obj, Cell)][0]
 			if cell.explored == 2:
-				count_explored +=1
+				count_explored += 1
 			if cell.explored == -1:
-				count_obstacle +=1
-		result = count_explored / (self.ncells*self.ncells - count_obstacle)
-		# if all cell has benn explored, stop the simulation
+				count_obstacle += 1
+		result = count_explored / (self.ncells * self.ncells - count_obstacle)
+		# if all cells have benn explored, stop the simulation
 		if result == 1:
 			print("Exploration Completed")
 			self.running = False
-		# keep going  with the exploration
+		# keep going with the exploration
 		else:
+			# DP I don't think you should cast it to string, if you want to print
+			# only a varibale the cast is implicit
 			print(str(result))
 
 	def run_model(self):
@@ -104,12 +121,16 @@ class ExplorationArea(Model):
 				self.running = False
 				break
 		#TODO
-		# implement search untill victim found
+		# implement search until victim found
+		# DP We do not know how many victims are out there, we should keep looking for them
+
+# DP I think that we can apply a bit of software engineering here. Because the size of the two classes 
+# will grow and having a file with more than 300 rows makes me sick, can we split in 2 files? Or Mesa gets angry?
 
 class Cell(Agent):
-	# 0 = not explored
+	# 0 not explored
 	# -1 obstacle
-	# 1 Cell ongoing
+	# 1 exploration ongoing
 	# 2 explored
 	def __init__(self, unique_id, model, pos, traversability, difficulty, explored, priority, utility):
 		super().__init__(unique_id, model)
@@ -142,21 +163,24 @@ class Robot(Agent):
 	# return the cell agent at the index given
 	def agent_get_cell(self, index):
 		tmp = self.model.grid.get_cell_list_contents(index)
-		cell = [obj for obj in tmp if isinstance(obj, Cell)][0]
+		cell = [obj for obj in tmp if isinstance(obj, Cell)][0] # DP why only the first element?
 		return cell
 
 	# add the sorrundings of the cell to the graph used for SP computation
 	def percept(self):
 		# iterate over the neighborhood
-		robot_seen = set(self.model.grid.get_neighborhood(self.pos, "moore", include_center=True, radius=self.radar_radius))
+		robot_seen = set(self.model.grid.get_neighborhood(self.pos, "moore", include_center = True, radius = self.radar_radius))
+		# DP with include center a robot can see itself?
 		for element in robot_seen:
-			cell_neigh = set(self.model.grid.get_neighborhood(element, "moore", include_center=False, radius=1))
-			inter = set.intersection(robot_seen, cell_neigh)
+			cell_neigh = set(self.model.grid.get_neighborhood(element, "moore", include_center = False, radius = 1))
+			inter = set.intersection(robot_seen, cell_neigh) # DP inter isn't definetely a clear variable name
 			for element2 in inter:
-				# s is suource node, d is destination node
+				# s is source node, d is destination node
 				s = element
 				d = element2
 				# check if d is reachable
+				# DP I think that cell_source and cell_destination can fit better
+				# maybe even element and element2 can be missleading
 				cell1 = self.agent_get_cell(s)
 				cell2 = self.agent_get_cell(d)
 				if cell1.traversability and cell2.traversability:
@@ -171,20 +195,25 @@ class Robot(Agent):
 				cell1 = self.agent_get_cell(d)
 				cell2 = self.agent_get_cell(s)
 				if cell1.traversability and cell2.traversability:
-					w = 1 + (cell1.difficulty // 4)
+					# DP I assume that traversability is a boolean
+					w = 1 + (cell1.difficulty // 4) # DP I don't get it
+					# DP I know that python is cool but wouldn't initilizing
+					# seen_graph in the __init__ better? In order to be more clearer 
+					# to the reader? 
 					if tuple([s, d]) not in self.model.seen_graph.edges():
-						self.model.seen_graph.add_edge(s, d, weight=w)
+						self.model.seen_graph.add_edge(s, d, weight = w)
 	
-	# retunr a list of tuples that represents the indexes of all the frontier cells
+	# return a list of tuples that represents the indexes of all the frontier cells
 	def find_frontier_cells(self):
 		frontier_cells = list()
-		# iterate over all cell of the grid
+		# iterate over all cells of the grid
+		# DP I think that we can rewrite it in a more fashonable way
 		for i in self.model.grid.coord_iter():
-			# pich the unexplored cell
+			# pick the unexplored cell
 			cell = self.agent_get_cell(i[1:])
 			if cell.explored == 0:
 				# iterate over the 1-radius neighborhood
-				for element in self.model.grid.get_neighborhood(i[1:], "moore", include_center=False, radius=1):
+				for element in self.model.grid.get_neighborhood(i[1:], "moore", include_center = False, radius = 1):
 					# search for a explored cell
 					cell2 = self.agent_get_cell(element)
 					if cell2.explored == 1 or cell2.explored == 2:
@@ -199,12 +228,12 @@ class Robot(Agent):
 		# Since the they are close, the unexplored cell has already been seen at least one time and added to the graph
 		# only consider seen cells that are not obstacles for the SP computation
 		
-		# list of tuples, the first element is the indexes of thecell, the second is the cost to get there
+		# list of tuples, the first element is the indexes of the cell, the second is the cost to get there
 		bids = list()
 		for i in frontier_cells:
 			# try to compute the shortest path to get to the cell. If the operation succedes, add the tuple to the bids list
 			try:
-				dist = nx.astar_path_length(self.model.seen_graph, self.pos, i, weight='weight')
+				dist = nx.astar_path_length(self.model.seen_graph, self.pos, i, weight = 'weight')
 				bids.append((i, dist))
 			except:
 				# if the path is not found, the cell is not considered
@@ -214,6 +243,9 @@ class Robot(Agent):
 		best_gain = -math.inf
 		best_cost = 0
 		# iterate over the possible destination
+		# DP I guess you're looking for the maximum, can't we simply sort the list 
+		# using key argument?
+		# somthing like this should mae the trick: sorted_by_length = sorted(list_, key=lambda x: (x[0], len(x[1]), float(x[1])))
 		for element in bids:
 			cell_index, cost = element
 			cell = self.agent_get_cell(cell_index)
@@ -231,6 +263,9 @@ class Robot(Agent):
 		return best_cell_index
 
 	def distance(self, cell1, cell2):
+		# DP why are calculating the distance in cells? 
+		# Wouldn't our distace be the sum of the weigths to get through cells?
+		# Am I missing something?
 		x1, y1 = cell1
 		x2, y2 = cell2
 		dx = abs(x2 - x1);
@@ -247,9 +282,14 @@ class Robot(Agent):
 
 	def step(self):
 		# if the agent has a move to get closer to the target, move towards it
+
+		# DP what values can assume target_path and target_cell? Are object with None values or empty tuples?
+
 		if self.target_path:
+			
 			## TODO
 			# consider path difficulty
+			
 			# update the last position
 			self.last_pos = self.pos
 			# move the agent
@@ -257,7 +297,9 @@ class Robot(Agent):
 			self.target_path = self.target_path[1:]
 			# find the cell that the robot can see and add to the graph
 			self.percept()
+
 			# TODO wifi range check
+		
 		else:
 			# if the agent has no move to do, but is on the target, explore
 			if self.target_cell:
@@ -269,7 +311,7 @@ class Robot(Agent):
 						cell.explored = 1
 					# anyway, explore
 					self.exploration_status += 1
-				# if the agent has ended the exploration, update the data of the cell (and self data)
+				# if the agent has finished the exploration, update the data of the cell (and self data)
 				else:
 					self.exploration_treshold = math.inf
 					self.exploration_status = 0
@@ -283,18 +325,21 @@ class Robot(Agent):
 				# find best cell
 				self.target_cell = self.find_best_cell(frontier_cells)
 				# if no frontier is avaiable, just stand still
-				# else, search for the sp and reduce utility near the targetted cell
+
+				# DP can waiting for frontiers bring to patological situation? 
+				# else, search for the sp and reduce utility near the target cell
+				
 				if (self.target_cell):
 					cell = self.agent_get_cell(self.target_cell)
 					self.exploration_treshold = cell.difficulty
 					# make the cell disgusting for other robots
 					cell.utility = -math.inf
 					# compute and store the most convinient path to get there
-					self.target_path = nx.astar_path(self.model.seen_graph, self.pos, self.target_cell, weight='weight')
+					self.target_path = nx.astar_path(self.model.seen_graph, self.pos, self.target_cell, weight = 'weight')
 					# the first element is the cell itself, so pop it
 					self.target_path = self.target_path[1:]
 					# reduce the utility of all the sorrundings cell if not visited yet
-					for element in self.model.grid.get_neighborhood(self.target_cell, "moore", include_center=False, radius=self.radar_radius):
+					for element in self.model.grid.get_neighborhood(self.target_cell, "moore", include_center = False, radius = self.radar_radius):
 						cell2 = self.agent_get_cell(element)
 						if cell2.explored == 0:
 						#cell.utility -= (1 - self.distance(self.target_cell, element) / self.radar_radius)
